02a650e6024824c5a8b7fe1243d846027721b4f6
Rushikesh Deshmukh <Rushikesh Deshmukh@LAPTOP-KJBUO1S7>
2021-06-27 14:57:56 +0530

    Test Two Commit

---
+++ src/test/java/com/DemoFramework/Test/TestTwo.java
@@ -0,0 +1,14 @@
+package com.DemoFramework.Test;
+
+import org.testng.annotations.Test;
+
+import com.DemoFramework.Utility.BaseClass;
+
+public class TestTwo extends BaseClass{
+	@Test
+	public void VeriftTC_002() {
+		System.out.println("QA");
+	}
+
+	
+}
